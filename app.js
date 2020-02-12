bf40391214b4bc6584af7fb4fa450bd00cae9574
Nick Mc <nbmc@protonmail.com>
2020-02-12 15:03:14 -0600

    completed 'timing out' section of drills.

--- app.js
+++ app.js
@@ -6,4 +6,25 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 function doStuff() {
     
+    function repeat(message) {
+        console.log(`${message}`);
+    }
+    repeat("hello world");
+    setTimeout(() => {
+        repeat("hello again");
+    }, 2000);
+
+    function getWords(w1, w2, w3, w4) {
+        console.log(w1);
+        setTimeout(() => {
+            console.log(w2);
+        }, 3000);
+        setTimeout(() => {
+            console.log(w3);
+        }, 2000);
+        setTimeout(() => {
+            console.log(w4);
+        }, 1000);
+    }
+    getWords("one", "two", "three", "four");
 }
